bc38e143814e4dc66019db8c589248d60b8215da
Camil Grabowski <camil.grabowski@uconn.edu>
2021-10-08 08:50:06 -0400

    bugged

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,27 @@
-const APP_PREFIX = 'FoodFest-';     
+const APP_PREFIX = 'FoodEvent-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./events.html",
-    "./tickets.html",
-    "./schedule.html",
-    "./assets/css/style.css",
-    "./assets/css/bootstrap.css",
-    "./assets/css/tickets.css",
-    "./dist/app.bundle.js",
-    "./dist/events.bundle.js",
-    "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
-  ];
+  "./index.html",
+  "./events.html",
+  "./tickets.html",
+  "./schedule.html",
+  "./assets/css/style.css",
+  "./assets/css/bootstrap.css",
+  "./assets/css/tickets.css",
+  "./dist/app.bundle.js",
+  "./dist/events.bundle.js",
+  "./dist/tickets.bundle.js",
+  "./dist/schedule.bundle.js"
+];
 
-self.addEventListener('install', function (e) {             // used self. -service workers run before the window object has even been created so the self keywork is to instantiate listeners on the service worker
+
+// Cache resources
+self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-          console.log('installing cache : ' + CACHE_NAME)
-          return cache.addAll(FILES_TO_CACHE)
-        })
-      )
-    })
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
